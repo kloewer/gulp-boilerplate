e7ece394d7b8becf11a66fc5c6697d86811110f4
Sebastian Prein <hi@sebastianprein.com>
2015-02-01 18:11:43 +0100

    fixed watch task.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,25 +5,25 @@ gulp.task('watch', [ 'images', 'styles', 'scripts', 'fonts' ], function(){
 
     // watch styles
     $.watch(appFiles.styles, function(files, cb) {
-        gulp.start('styles', cb);
+        gulp.start('styles');
     })
       .on('change', function(event) { changeEvent(event); });
 
     // watch for JavaScript changes
     $.watch(appFiles.scripts, function(files, cb) {
-        gulp.start('scripts', cb);
+        gulp.start('scripts');
     })
       .on('change', function(event) { changeEvent(event); });
 
     // watch images
     $.watch(appFiles.images, function(files, cb) {
-      gulp.start('images', cb);
+      gulp.start('images');
     })
       .on('change', function(event) { changeEvent(event); });
 
     // watch fonts
     $.watch(appFiles.fonts, function(files, cb) {
-      gulp.start('fonts', cb);
+      gulp.start('fonts');
     })
       .on('change', function(event) { changeEvent(event); });
 });
