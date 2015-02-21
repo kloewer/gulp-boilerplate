d97287a8eef315fd0b85c2b8ed8c447dacd336aa
Sebastian Prein <hi@sebastianprein.com>
2015-02-21 12:48:54 +0100

    added version tag to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,7 @@
  * Each task has been broken out into its own file in `gulp/tasks`. any file in
  * that folder gets automatically required by the loop in `./gulp/index.js`.
  * to add a new task, simply add a new task file to gulp/tasks.
+ *
+ * @version 1.0.0
  */
 require('./gulp');
