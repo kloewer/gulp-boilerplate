13e28ed8c0c771f32a4541d9685bea9478990dc9
laurids <laurids.duellmann@gmail.com>
2015-10-31 12:30:29 +0100

    upgraded NPM modules, removed gulp-combine-media-queries, added gulp-merge-media-queries, removed SVGsprites examples, fixes bugs when using node >= 4.0

--- gulp/tasks/svgsprites.js
+++ gulp/tasks/svgsprites.js
@@ -1,23 +1,9 @@
 svgSprite               = require('gulp-svg-sprite');
 
 config                  = {
-    shape               : {
-        dimension       : {         // Set maximum dimensions
-            maxWidth    : 32,
-            maxHeight   : 32
-        },
-        spacing         : {         // Add padding
-            padding     : 10
-        }
-    },
-    mode                : {
-        view            : {         // Activate the «view» mode
-            bust        : false,
-            render      : {
-                scss    : false      // Activate Sass output (with default options)
-            }
-        },
-        symbol          : true      // Activate the «symbol» mode
+    mode                    : {
+        inline              : true,     // Prepare for inline embedding
+        symbol              : true      // Create a «symbol» sprite
     }
 };
 
